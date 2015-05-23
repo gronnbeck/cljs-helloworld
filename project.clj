8ca26c0882c8ace0ab05155d98a021a6bc5410c3
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-23 10:04:44 +0200

    Add all the stuff remove them later

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3269"]]
 
-  :node-dependencies [[source-map-support "0.2.8"]]
+  :node-dependencies [[source-map-support "0.2.8"]
+                      [express "4.11.1"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
             [lein-npm "0.4.0"]]
